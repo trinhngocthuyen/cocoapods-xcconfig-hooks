a9dce196914d0ba9c185c7ca02f3238d89ea2e43
Thuyen Trinh <trinhngocthuyen@gmail.com>
2022-12-06 08:56:57 +0700

    Add example project

---
+++ examples/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Thuyen Trinh on 06/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
